9d14da737f759baa441c4ad69815fb2f7b93dd10
Vladyslav Hubanov <155400906+vlad-gby@users.noreply.github.com>
2024-01-26 21:29:58 +0200

    Create a working script with basic styling

--- script.js
+++ script.js
@@ -12,12 +12,40 @@ function getComputerChoice(){
 
 function playRound(){
   const computerSelection = getComputerChoice();
-  if (!computerSelection) return 'Enter something'
-  const playerSelection = prompt('Enter Rock, Paper or Scissors').toLowerCase();
+  let playerSelection;
 
-  let playerSelectionCaps = playerSelection.at(0).toUpperCase() + playerSelection.slice(1);
-  let computerSelectionCaps = computerSelection.at(0).toUpperCase() + computerSelection.slice(1);
+  // CHECK IF INPUT IS VALID-----------
+  // x controls the loop 
+  // a checks what to display in the prompt
+  for(let x = 0, a = 0; x != 1;){
+    switch (a){
+      case 0:
+        playerSelection = prompt('Enter Rock, Paper or Scissors', '') || 'close';
+      break;
+      case 1:
+        playerSelection = prompt('May be you got a typo.\nPlease, enter Rock, Paper or Scissors', '') || 'close';
+      break;
+      case -1:
+        playerSelection = prompt('You didn\'t enter anything.\nPlease, enter Rock, Paper or Scissors', '') || 'close';
+      break;
+    }
+    if (playerSelection == 'close')return;
+    playerSelection = playerSelection.toLowerCase();
 
+    if (playerSelection == '') {a = -1; continue;}
+    switch (playerSelection){
+      case 'rock': x = 1; break;
+      case 'paper': x = 1; break;
+      case 'scissors': x = 1; break;
+      default: a = 1;
+    }
+  }
+  
+
+  const playerSelectionCaps = playerSelection.at(0).toUpperCase() + playerSelection.slice(1);
+  const computerSelectionCaps = computerSelection.at(0).toUpperCase() + computerSelection.slice(1);
+
+  // WHO WINS?-------------------------
   if (playerSelection === computerSelection){
     return ('It\'s a tie!')
   }else if (playerSelection == 'rock' && computerSelection == 'paper'){
@@ -32,36 +60,40 @@ function playRound(){
     return (`You lose! ${computerSelectionCaps} beats ${playerSelectionCaps}`)
   }else if (playerSelection == 'scissors' && computerSelection == 'paper'){
     return (`You win! ${playerSelectionCaps} beats ${computerSelectionCaps}`)
-  }else{
-    return null;
   }
-  
+
 }
 
+
+// GAME OF 5 ROUNDS--------------------
 function playGame(){
-  let wins = 0;
-  let looses = 0;
+  let w = 0;
+  let l = 0;
   
-  for (i = 0; i <= 5;){
-    playRound();
-    if (!playRound){
-      return ('Please, enter either Rock, Paper or Scissors')
-    }
+  for (i = 0; i < 5;){
+    let result = playRound();
 
-    if (String(playRound).at(5) == 'l'){
-      i++
-      return (`Wins: ${wins} Looses: ${looses++}`)
-    } else if (String(playRound).at(5) == 'w'){
-      i++
-      return (`Wins: ${wins++} Looses: ${looses}`)
-    }
+    if(result === undefined) return;
+    
+    if (result.at(4) == 'l') l++;
+    if (result.at(4) == 'w') w++;
+    i++
   }
 
-  if (wins > looses){
-    return (`\n ${wins} wins VS ${looses} looses. You WON! `)
-  }else{
-    return (`\n ${wins} wins VS ${looses} looses. You lost! `)
+
+  let winpl = w == 1 ? 'win' : 'wins'
+  let loosepl = l == 1 ? 'loose' : 'looses'
+  let tiepl = 5 - (w+l) == 1 ? 'tie' : 'ties' 
+
+
+  if (w > l){
+    return (`\n ${w} ${winpl} VS ${l} ${loosepl}, and ${5 - (w+l)} ${tiepl}. You WON!`)
+  }else if(w < l){
+    return (`\n ${w} ${winpl} VS ${l} ${loosepl}, and ${5 - (w+l)} ${tiepl}. You lost!`)
+  } else if(w == l){
+    return (`\n ${w} ${winpl} VS ${l} ${loosepl}, and ${5 - (w+l)} ${tiepl}. It\'s a TIE!`)
   }
+
 }
 
 
